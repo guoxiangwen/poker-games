8c43de64522cfff05620601b6dd57e202169797f
guoxiangwen <gxw1990**>
2017-03-29 11:43:48 +0800

    完成card组件

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,125 +1,142 @@
+
 <template>
-    <div class="card" v-bind:class="back?'suit_back':'suit_clubs'">
-        <span v-bind:text="back?'':'options.text'"></span>
+    <div class="card" v-bind:class="back?'card suit_back':check_suit()" @click="handleClick()">
+        <span v-text="back?'':text"></span>
     </div>
 </template>
 
-
+ 
 <script>
-export default {
-    name: 'card',
-    data(){
-        return{
-            back:false
-        }
-    },
-    props:{
-        options:{
-            type:Object,
-            default(){
-                return{
-                    type:"suit_back",
-                    text:"7"
+    export default {
+        created() {
+            this.check_suit();
+        },
+        name: 'card',
+        data() {
+            return {
+                back: true
+            }
+        },
+        props: {
+            type: {
+                required: true
+            },
+            text: {
+                default: "A"
+            }
+        },
+        methods: {
+            handleClick: function() {
+                this.back = !this.back
+            },
+            check_suit: function() {
+                switch (this.type) {
+                    case "diamonds":
+                        return "card suit_diamonds";
+                        break;
+                    case "hearts":
+                        return "card suit_hearts"
+                        break;
+                    case "clubs":
+                        return "card suit_clubs"
+                        break;
+                    case "c":
+                        return "card suit_spades";
+                        break;
+                    default:
+                        return "suit_spades";
+                        break;
                 }
             }
         }
-    },
-    method:{
-
     }
-}
 </script>
 
 <style lang="less" scoped>
-.card {
-    position: relative;
-    margin-right: 10px;
-    width: 150px;
-    height: 220px;
-    border-radius: 10px;
-    background: #fff;
-    -webkit-box-shadow: 3px 3px 7px rgba(0, 0, 0, 0.3);
-    box-shadow: 3px 3px 7px rgba(0, 0, 0, 0.3);
-    &:hover {
-        cursor: pointer;
+    .card {
+        position: relative;
+        margin-right: 10px;
+        width: 150px;
+        height: 220px;
+        border-radius: 10px;
+        background: #fff;
+        -webkit-box-shadow: 3px 3px 7px rgba(0, 0, 0, 0.3);
+        box-shadow: 3px 3px 7px rgba(0, 0, 0, 0.3);
+        &:hover {
+            cursor: pointer;
+        }
+        span {
+            // margin-left: 45px;
+            line-height: 220px;
+            vertical-align: middle;
+            font: 100px/220px Georgia, Times New Roman, serif;
+        }
     }
-    span {
-        margin-left: 45px;
-        line-height: 220px;
-        vertical-align: middle;
-        font: 100px/220px Georgia, Times New Roman, serif;
+    
+    .suit_content(@content, @color) {
+        content: @content;
+        color: @color
     }
-}
-
-
-.suit_content(@content, @color) {
-    content: @content;
-    color: @color
-}
-
-.suit_position_before() {
-    font-size: 35px;
-    position: absolute;
-    top: 5px;
-    left: 5px;
-}
-
-.suit_position_after() {
-    font-size: 35px;
-    position: absolute;
-    bottom: 5px;
-    right: 5px;
-}
-
-
-.suit_diamonds {
-    &:before {
-        .suit_position_before();
-        .suit_content("♦", #ff0000)
+    
+    .suit_position_before() {
+        font-size: 35px;
+        position: absolute;
+        top: 5px;
+        left: 5px;
     }
-    &:after {
-        .suit_position_after();
-        .suit_content("♦", #ff0000)
+    
+    .suit_position_after() {
+        font-size: 35px;
+        position: absolute;
+        bottom: 5px;
+        right: 5px;
     }
-}
-
-
-.suit_hearts {
-    &:before {
-        .suit_position_before();
-        .suit_content("♥", #ff0000)
+    
+    .suit_diamonds {
+        &:before {
+            .suit_position_before();
+            .suit_content("♦", #ff0000)
+        }
+        &:after {
+            .suit_position_after();
+            .suit_content("♦", #ff0000)
+        }
     }
-    &:after {
-        .suit_position_after();
-        .suit_content("♥", #ff0000)
+    
+    .suit_hearts {
+        &:before {
+            .suit_position_before();
+            .suit_content("♥", #ff0000)
+        }
+        &:after {
+            .suit_position_after();
+            .suit_content("♥", #ff0000)
+        }
     }
-}
-
-
-.suit_clubs {
-    &:before {
-        .suit_position_before();
-        .suit_content("♣", #000)
+    
+    .suit_clubs {
+        &:before {
+            .suit_position_before();
+            .suit_content("♣", #000)
+        }
+        &:after {
+            .suit_position_after();
+            .suit_content("♣", #000)
+        }
     }
-    &:after {
-        .suit_position_after();
-        .suit_content("♣", #000)
+    
+    .suit_spades {
+        &:before {
+            .suit_position_before();
+            .suit_content("♠", #000)
+        }
+        &:after {
+            .suit_position_after();
+            .suit_content("♠", #000)
+        }
     }
-}
-
-
-.suit_spades {
-    &:before {
-        .suit_position_before();
-        .suit_content("♠", #000)
+    
+    .suit_back {
+        background: #ff0000;
     }
-    &:after {
-        .suit_position_after();
-        .suit_content("♠", #000)
-    }
-}
-
-.suit_back {
-    background: #ff0000;
-}
 </style>
--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,24 +1,53 @@
 <template>
-    <div id="game">
-    {{title}}
-    <card></card>
+    <div id="game" class="app-container">
+        <card text="J" type="diamonds"></card>
+        <card text="Q" type="hearts"></card>
+        <card text="K" type="clubs"></card>
+        <card text="A" type="spades"></card>
     </div>
 </template>
 
 <script>
-import Card from './Card.vue'
-export default {
-    name: 'game',
-    data() {
-        return {
-            title: 'poker'
+    import Card from './Card.vue'
+    export default {
+        name: 'game',
+        data() {
+            return {
+                title: 'poker'
+            }
+        },
+        components: {
+            Card
         }
-    },
-    components:{
-        Card
     }
-}
 </script>
 
 <style lang="less" scoped>
+    .app-container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    .card-container {
+        margin-top: 100px;
+        box-sizing: border-box;
+        width: 100%;
+        display: flex;
+        flex-flow: row;
+        flex-wrap: wrap;
+        // align-items: center;
+        justify-content: space-between;
+    }
+    main>div {
+        box-sizing: border-box;
+        width: 100%;
+        min-height: 500px;
+        padding: 20px 20px;
+        display: flex;
+        flex-flow: row;
+        flex-wrap: wrap;
+        // align-items: center;
+        justify-content: space-between;
+    }
+}
 </style>
